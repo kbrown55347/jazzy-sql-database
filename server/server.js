1a93a0e6707665ea55c79d21760a309f3a1fa8f6
Kelsey Brown <kbrown55347@gmail.com>
2021-11-16 18:43:49 -0600

    updated \song POST route to store data in database

--- server/server.js
+++ server/server.js
@@ -28,43 +28,6 @@ app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
 
-// // TODO - Replace static content with a database tables
-// const artistList = [
-//     {
-//         name: 'Ella Fitzgerald',
-//         birthdate: '04-25-1917'
-//     },
-//     {
-//         name: 'Dave Brubeck',
-//         birthdate: '12-06-1920'
-//     },
-//     {
-//         name: 'Miles Davis',
-//         birthdate: '05-26-1926'
-//     },
-//     {
-//         name: 'Esperanza Spalding',
-//         birthdate: '10-18-1984'
-//     },
-// ]
-// const songList = [
-//     {
-//         title: 'Take Five',
-//         length: '5:24',
-//         released: '1959-09-29'
-//     },
-//     {
-//         title: 'So What',
-//         length: '9:22',
-//         released: '1959-08-17'
-//     },
-//     {
-//         title: 'Black Gold',
-//         length: '5:17',
-//         released: '2012-02-01'
-//     }
-// ];
-
 app.get('/artist', (req, res) => {
     const sqlText = `SELECT * FROM artist 
         ORDER BY birthdate DESC;`
@@ -112,8 +75,24 @@ app.get('/song', (req, res) => {
 });
 
 app.post('/song', (req, res) => {
-    songList.push(req.body);
-    res.sendStatus(201);
+    const newSong = req.body;
+    const sqlText = (`
+    INSERT INTO "song"
+        ("title", "length", "released"
+    VALUES
+        ($1, $2, $3);
+    `)
+    const sqlValues = [
+        newSong.title,
+        newSong.length,
+        newSong.released
+    ];
+    console.log(sqlText);
+    pool.query(sqlText, sqlValues)
+        .then((dbRes) => {
+            res.sendStatus(201);
+        })
+        .catch((dbErr) => {
+            console.error(dbErr);
+        });
 });
-
-
