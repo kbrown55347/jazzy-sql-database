b495276056b2d48c87749515e407eaa1b124e052
Kelsey Brown <kbrown55347@gmail.com>
2021-11-16 18:21:24 -0600

    updated \artist GET route to get data from database

--- server/server.js
+++ server/server.js
@@ -1,56 +1,79 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 const PORT = 5000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+// require pg module
+const pg = require('pg');
+// create pool object constructor
+const Pool = pg.Pool;
+// create our pool object using above constructor
+const pool = new Pool({
+    database: 'jazzy_sql',
+    host: 'Localhost'
+});
+
+pool.on('connect', () => {
+    console.log('Postgresql connected');
+});
+
+pool.on('error', (error) => {
+    console.log('Error with postgres pool', error)
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
 
-// TODO - Replace static content with a database tables
-const artistList = [ 
-    {
-        name: 'Ella Fitzgerald',
-        birthdate: '04-25-1917'
-    },
-    {
-        name: 'Dave Brubeck',
-        birthdate: '12-06-1920'
-    },       
-    {
-        name: 'Miles Davis',
-        birthdate: '05-26-1926'
-    },
-    {
-        name: 'Esperanza Spalding',
-        birthdate: '10-18-1984'
-    },
-]
-const songList = [
-    {
-        title: 'Take Five',
-        length: '5:24',
-        released: '1959-09-29'
-    },
-    {
-        title: 'So What',
-        length: '9:22',
-        released: '1959-08-17'
-    },
-    {
-        title: 'Black Gold',
-        length: '5:17',
-        released: '2012-02-01'
-    }
-];
+// // TODO - Replace static content with a database tables
+// const artistList = [
+//     {
+//         name: 'Ella Fitzgerald',
+//         birthdate: '04-25-1917'
+//     },
+//     {
+//         name: 'Dave Brubeck',
+//         birthdate: '12-06-1920'
+//     },
+//     {
+//         name: 'Miles Davis',
+//         birthdate: '05-26-1926'
+//     },
+//     {
+//         name: 'Esperanza Spalding',
+//         birthdate: '10-18-1984'
+//     },
+// ]
+// const songList = [
+//     {
+//         title: 'Take Five',
+//         length: '5:24',
+//         released: '1959-09-29'
+//     },
+//     {
+//         title: 'So What',
+//         length: '9:22',
+//         released: '1959-08-17'
+//     },
+//     {
+//         title: 'Black Gold',
+//         length: '5:17',
+//         released: '2012-02-01'
+//     }
+// ];
 
 app.get('/artist', (req, res) => {
-    console.log(`In /songs GET`);
-    res.send(artistList);
+    const sqlText = 'SELECT * FROM artist ORDER BY birthdate DESC;'
+    pool.query(sqlText)
+    .then((dbRes) => {
+        const artistsFromDb = dbRes.rows;
+        res.send(artistsFromDb);
+    }).catch((dbErr) => {
+        console.error(dbErr);
+    });
 });
 
 app.post('/artist', (req, res) => {
